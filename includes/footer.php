14b844fc6e19bea383d9b4fa5847157dbe0d4c8d
Patroclo <hapaza.221ds02@istta.edu.pe>
2024-12-03 12:36:15 -0500

    first-commit

---
+++ includes/footer.php
@@ -0,0 +1,32 @@
+<!-- BOOTSTRAP 4 SCRIPTS -->
+<footer>
+  <div class="contact">
+    <h1>Contactanos</h1>
+    <p>Somos una miniempresa con el proposito de generar soluciones informacticas para nuestros clientes.</p>
+    <ul>
+      <li><a href="/"></a></li>
+      <li><a href="/"></a></li>
+      <li><a href="/"></a></li>
+      <li><a href="/"></a></li>
+    </ul>
+  </div>
+  <div class="container">
+    <!-- <div class="box"> -->
+      <a class="box" href="https://google.com" target="_blank">
+        <img src="assets/img/web-page.png" alt="web">
+        <p>google.com</p>
+      </a>
+    <!-- </div> -->
+    <!-- <div class="box"> -->
+      <a class="box" href="mailto:chemoranxv5@gmail.com">
+        <img src="assets/img/email.png" alt="email">
+        <p>google.com</p>
+      </a>
+    <!-- </div> -->
+  </div>
+</footer>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
+</body>
+</html>
